3cc2cd6fa9e16b51baadd4dadd35e7d16b8d419e
Alejandro-Echaniz <a.echaniz02@gmail.com>
2022-10-30 13:37:24 -0400

    CI: adding files for web_scrapping

---
+++ data_acquisition/web_scrapper.py
@@ -0,0 +1,6 @@
+# Implementation of web scrapping lies here, using libraries of beautiful soup and requests to gain data
+# NOTE: please install requests and beautiful soup
+
+import requests
+
+r = requests.get('https://www.gardenia.net/plants/plant-types')
