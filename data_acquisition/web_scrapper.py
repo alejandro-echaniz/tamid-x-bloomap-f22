6d98aeaf72e13a3e9c71ddea50aba8c8236c777f
Alejandro-Echaniz <a.echaniz02@gmail.com>
2022-10-30 13:40:45 -0400

    doc: adding additional documentation regarding project description

--- data_acquisition/web_scrapper.py
+++ data_acquisition/web_scrapper.py
@@ -1,5 +1,7 @@
 # Implementation of web scrapping lies here, using libraries of beautiful soup and requests to gain data
-# NOTE: please install requests and beautiful soup
+# NOTE: please install requests and beautiful soup locally
+# requests --> gives us the URL's directly to work with for beautiful soup web scrapping
+# Beautiful Soup --> manually scrapes the information through the requests objects created
 
 import requests
 
