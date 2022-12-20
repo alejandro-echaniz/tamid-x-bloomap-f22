0cb549d60efff64323bd77b5ca8fc96b660727d5
Alejandro Echaniz <a.echaniz02@gmail.com>
2022-12-19 21:34:11 -0500

    chore: removing unnecessary directory

--- data_acquisition/web_scrapper.py
+++
@@ -1,8 +0,0 @@
-# Implementation of web scrapping lies here, using libraries of beautiful soup and requests to gain data
-# NOTE: please install requests and beautiful soup locally
-# requests --> gives us the URL's directly to work with for beautiful soup web scrapping
-# Beautiful Soup --> manually scrapes the information through the requests objects created
-
-import requests
-
-r = requests.get('https://www.gardenia.net/plants/plant-types')
